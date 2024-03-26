cd84059aab12c41fdaeac2c39ce84cc3fa1d33db
Zainab Amir <zainab.amir@arbisoft.com>
2024-03-26 13:52:32 +0500

    chore: update configs for authn component
    - Added pull request template
    - Added eslint rules for imports and indents

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,45 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { createConfig } = require('@openedx/frontend-build');
+const { createConfig } = require('@edx/frontend-build');
 
-module.exports = createConfig('eslint');
+module.exports = createConfig('eslint', {
+  rules: {
+    indent: [
+      'error',
+      2,
+      { ignoredNodes: ['TemplateLiteral', 'SwitchCase'] },
+    ],
+    'jsx-a11y/label-has-associated-control': ['error', {
+      labelComponents: [],
+      labelAttributes: [],
+      controlComponents: [],
+      assert: 'htmlFor',
+      depth: 25,
+    }],
+    'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-dom|react-redux)',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+});
