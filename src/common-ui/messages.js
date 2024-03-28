c7a2ba06ae88177ae89cb0f4d72bba2de96975c0
Zainab Amir <zainab.amir@arbisoft.com>
2024-03-28 12:10:31 +0500

    feat: add social auth buttons (#3)
    Added a new component for social auth providers used on edX. This
    component can handle both login and registration page requirements.
    
    VAN-1883

---
+++ src/common-ui/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  socialAuthProviderSignupTitle: {
+    id: 'social.auth.provide.signup.title',
+    defaultMessage: 'Sign up with {providerName}',
+    description: 'Title that appears on the social sign up buttons i.e Sign up with Google',
+  },
+  socialAuthProviderSigninTitle: {
+    id: 'social.auth.provide.signin.title',
+    defaultMessage: 'Sign in with {providerName}',
+    description: 'Title that appears on the social sign in buttons i.e Sign in with Google',
+  },
+});
+
+export default messages;
