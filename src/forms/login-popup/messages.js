e5b30e04552613513bc300c401d14e3dd48b4a3a
Zainab Amir <zainab.amir@arbisoft.com>
2024-04-26 14:07:34 +0500

    feat: fix design issues

--- src/forms/login-popup/messages.js
+++ src/forms/login-popup/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   loginFormHeading1: {
     id: 'login.form.heading.1',
-    defaultMessage: 'Login',
+    defaultMessage: 'Sign in',
     description: 'Login form main heading',
   },
   loginFormSignInButton: {
@@ -17,14 +17,24 @@ const messages = defineMessages({
     description: 'Button text for forgot password',
   },
   loginFormRegistrationHelpText: {
-    id: 'login.form.sign.up.helping.text',
+    id: 'login.form.sign.up.help.text',
     defaultMessage: 'Don’t have an account yet?',
     description: 'Sign up link help text',
   },
   loginFormRegistrationLink: {
     id: 'login.form.sign.up.link.text',
-    defaultMessage: 'Create an account',
+    defaultMessage: 'Create account',
     description: 'Text that appears on the registration button',
+  },
+  loginFormSchoolAndOrganizationHelpText: {
+    id: 'login.form.school.and.organization.help.text',
+    defaultMessage: 'Have an account through school or organization?',
+    description: 'Label for school and organization login link',
+  },
+  loginFormSchoolAndOrganizationLink: {
+    id: 'login.form.school.and.organization.link',
+    defaultMessage: 'Sign in with your credentials',
+    description: 'Text that appears on school and organization login link',
   },
   loginFormEmailFieldLabel: {
     id: 'login.form.email.field.label',
