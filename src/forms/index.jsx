0f9a64f31b28edd57a568544768d861e3ba2877e
Syed Sajjad Hussain Shah <52817156+syedsajjadkazmii@users.noreply.github.com>
2024-05-17 18:18:21 +0500

    fix: move TOSAndHonorCode to register form and fix sso skeletons (#32)

--- src/forms/index.jsx
+++ src/forms/index.jsx
@@ -2,7 +2,6 @@ export { default as RegistrationForm } from './registration-popup';
 export { default as registerReducer } from './registration-popup/data/reducers';
 export { default as registerSaga } from './registration-popup/data/sagas';
 export { storeName as registerStoreName } from './registration-popup/data/reducers';
-export { default as HonorCodeAndPrivacyPolicyMessage } from './registration-popup/honorCodeAndTOS';
 
 export { default as LoginForm } from './login-popup';
 export { default as loginReducer } from './login-popup/data/reducers';
