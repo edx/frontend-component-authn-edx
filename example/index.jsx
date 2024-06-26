9efc4c0496556a23bdaffbe70d5002e2e59c7bab
Zainab Amir <zainab.amir@arbisoft.com>
2024-06-26 21:11:14 +0500

    fix: hyperlink related issues

--- example/index.jsx
+++ example/index.jsx
@@ -25,12 +25,13 @@ initialize({
       mergeConfig({
         AUTHN_ALGOLIA_APP_ID: process.env.AUTHN_ALGOLIA_APP_ID || '',
         AUTHN_ALGOLIA_SEARCH_API_KEY: process.env.AUTHN_ALGOLIA_SEARCH_API_KEY || '',
-        TOS_AND_HONOR_CODE: process.env.TOS_AND_HONOR_CODE || '',
+        INFO_EMAIL: process.env.INFO_EMAIL || '',
+        LOGIN_ISSUE_SUPPORT_LINK: process.env.LOGIN_ISSUE_SUPPORT_LINK || '',
+        ONBOARDING_COMPONENT_ENV: process.env.ONBOARDING_COMPONENT_ENV || '',
         PASSWORD_RESET_SUPPORT_LINK: process.env.PASSWORD_RESET_SUPPORT_LINK || '',
         PRIVACY_POLICY: process.env.PRIVACY_POLICY || '',
-        INFO_EMAIL: process.env.INFO_EMAIL || '',
+        TOS_AND_HONOR_CODE: process.env.TOS_AND_HONOR_CODE || '',
         USER_RETENTION_COOKIE_NAME: process.env.USER_RETENTION_COOKIE_NAME || '',
-        ONBOARDING_COMPONENT_ENV: process.env.ONBOARDING_COMPONENT_ENV || '',
       });
     },
   },
