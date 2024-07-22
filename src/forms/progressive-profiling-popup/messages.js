2128ac2d3aabe60315076ee79c79b0335367dcee
Attiya Ishaque <atiya.ishaq@arbisoft.com>
2024-07-22 15:23:17 +0500

    fix: update country msg on skip button (#120)

--- src/forms/progressive-profiling-popup/messages.js
+++ src/forms/progressive-profiling-popup/messages.js
@@ -36,10 +36,9 @@ const messages = defineMessages({
     defaultMessage: 'Select a valid option',
     description: 'Error text appers on the country field when country is not selected and the user submit the form',
   },
-  // TODO update error message copy here when design team will provide it
   progressiveProfilingCountryFieldBlockingErrorMessage: {
     id: 'progressive.profiling.country.field.error.message',
-    defaultMessage: 'To proceed, please save your country of residence',
+    defaultMessage: 'Please click "Submit" to save changes in "Country of residence" field.',
     description: 'Error msg for country field when the user country is not detected on registration step and user want to skip progressive profiling form',
   },
   progressiveProfilingDataCollectionTitle: {
